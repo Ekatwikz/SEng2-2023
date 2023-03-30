68fe9da40ee4405e30a063d5967f956b639b1f6a
Ib0-shayeb <ibrahimowich1@gmail.com>
2023-03-31 00:42:20 +0200

    basic login + homepage

---
+++ frontend/seng/components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            Copyright 2023 Something
+        </div>
+    )
+}
+
+export default Footer;
---
+++ frontend/seng/components/navbar.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>logo</h1>
+            </div>
+            <Link href="/">Home</Link>
+            <Link href="/login">Login</Link>
+        </nav>
+    )
+}
+
+export default Navbar;
